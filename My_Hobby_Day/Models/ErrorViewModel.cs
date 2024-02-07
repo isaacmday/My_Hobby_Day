825b6f294355d6af2c8120d46c4a6c91d8e25cd0
Isaac Day <dayisaacm81@gmail.com>
2024-02-07 12:55:16 -0700

    Add project files.

---
+++ My_Hobby_Day/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Hobby_Day.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
